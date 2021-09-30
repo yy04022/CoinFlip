22eee171a5938c2926a19bbb149de4646cbf4914
Jinyu Wang <ellie5201314@gmail.com>
2021-09-29 20:05:07 -0400

    Class Exist

--- CoinFlip/ViewController.swift
+++ CoinFlip/ViewController.swift
@@ -14,10 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     
 
+    var imageList = [UIImage(named: "Heads"), UIImage(named: "Tails")]
+    
+    //Random Position is generated when you launch the app
+    //Generate random position when you click on the botton
     @IBAction func click(_ sender: UIButton) {
     
-        image.image = UIImage(named: "Heads")
-    
+        //who.what=value
+            //access value from the array
+        //Function-->Random || How to use random library in xcode
+        let randomPosition = Int.random(in: 0...1)
+        image.image = imageList[randomPosition]
     }
     
     
